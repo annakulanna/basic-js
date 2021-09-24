468938ada818b381ffaa3386cf5c82dc88c55b42
Anna Kulesh <annakulanna@gmail.com>
2021-09-24 12:46:11 +0300

    feat: implement extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  if (arguments.length === 1)
+    return str;
+
+  if (!options.hasOwnProperty('separator'))
+    options.separator = '+';
+  if (!options.hasOwnProperty('additionSeparator'))
+    options.additionSeparator = '|';
+  if (!options.hasOwnProperty('repeatTimes'))
+    options.repeatTimes = 1;
+  if (!options.hasOwnProperty('additionRepeatTimes'))
+    options.additionRepeatTimes = 1;
+  if (!options.hasOwnProperty('addition') || options.hasOwnProperty('addition') === null)
+    options.addition = '';
+
+  let arr = [];
+  for (let i = 0; i < options.repeatTimes; i++) {
+    let par = [];
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      par.push(String(options.addition));
+    }
+    arr.push(str + par.join(options.additionSeparator));
+  }
+
+  return arr.join(options.separator)
+
 }
